9b926a50ebe573ceb4c60ab1491fe05d0900d77f
akshat0047 <pande.akshat21@gmail.com>
2019-04-06 08:33:37 +0530

    added basic template

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Canvas-draw
 A user end implementation of JavaScript Canvas
+
+[LIVE](https://akshat0047.github.io/Canvas-draw/)
---
+++ main.js
@@ -0,0 +1,105 @@
+var canvasDiv = document.getElementById('canva-container');
+var canvas = document.createElement('canvas');
+canvas.setAttribute('id', 'drawCanvas');
+canvasDiv.appendChild(canvas);
+if (typeof G_vmlCanvasManager != 'undefined') {
+    canvas = G_vmlCanvasManager.initElement(canvas);
+}
+
+var context = canvas.getContext('2d');
+var canvaDraw = document.getElementById("drawCanvas");
+
+initialize();
+
+function initialize() {
+    window.addEventListener('resize', resizeCanvas, false);
+    resizeCanvas();
+}
+
+function redrawCanvas(w, h) {
+    context.strokeStyle = 'black';
+    context.lineWidth = '5';
+    context.strokeRect(0, 0, w, h);
+}
+
+function resizeCanvas() {
+    canvaDraw.width = document.getElementById("canva-container").clientWidth * 0.9;
+    canvaDraw.height = document.getElementById("canva-container").clientHeight;
+    redrawCanvas(canvaDraw.width, canvaDraw.height);
+}
+
+
+//Draw
+
+var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var adjusty = document.getElementById("canva-container").clientHeight * 0.20;
+var paint;
+
+function addClick(x, y, dragging) {
+    clickX.push(x);
+    clickY.push(y);
+    clickDrag.push(dragging);
+}
+
+$('#drawCanvas').mousedown(function (e) {
+    var mouseX = e.pageX - this.offsetLeft;
+    var mouseY = e.pageY - adjusty;
+    paint = true;
+    addClick(mouseX, mouseY);
+    redraw();
+});
+
+$('#drawCanvas').mousemove(function (e) {
+    if (paint) {
+        addClick(e.pageX - this.offsetLeft, e.pageY - adjusty - this.offsetTop, true);
+        redraw();
+    }
+});
+
+$('#drawCanvas').mouseup(function (e) {
+    paint = false;
+});
+
+$('#drawCanvas').mouseleave(function (e) {
+    paint = false;
+});
+
+
+function palette() {
+    colors = ["#000000", "#f40919", "#f408a1", "#f408a1", "#a108f4", "#a108f4", "#f4f408", "#f40b08", "#08f4d0", "#08f4d0"];
+    for (var i = 0; i < 10; i++) {
+        document.getElementById("colorstream").innerHTML += "<span class='color-select'></span>";
+        document.getElementsByClassName("color-select")[i].style.backgroundColor = colors[i];
+    }
+}
+palette();
+
+var linecolor;
+var setcolor = document.querySelectorAll(".color-select");
+for (i = 0; i < setcolor.length; i++) {
+    setcolor[i].addEventListener("click", function () {
+        linecolor = this.style.backgroundColor;
+    });
+}
+
+
+
+function redraw() {
+    context.lineJoin = "round";
+    context.lineWidth = 5;
+
+    for (var i = 0; i < clickX.length; i++) {
+        context.beginPath();
+        if (clickDrag[i] && i) {
+            context.moveTo(clickX[i - 1], clickY[i - 1]);
+        } else {
+            context.moveTo(clickX[i] - 1, clickY[i]);
+        }
+        context.lineTo(clickX[i], clickY[i]);
+        context.closePath();
+        context.strokeStyle = linecolor;
+        context.stroke();
+    }
+}
